c398ac23d47789b410ddc50df9f5a0eee9725947
naveen <naveenmaturu@gmail.com>
2017-08-10 02:32:44 +0530

    Creating Hubble View in angular4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+   a;
+  private apiURL = "assets/table.json"
+
+  data: any ={};
+
+  constructor(public http: Http){
+
+	  console.log('hello world');
+	  this.getData();
+	  this.getNaveen();
+  }
+
+  getData(){
+
+	  return this.http.get(this.apiURL)
+	 .map(res => res.json());
+  }
+  getNaveen(){
+	  this.getData().subscribe(data =>{
+	  //console.log(JSON.stringify(data));
+		  this.a = data
+		   console.log(this.a);
+
+	  })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	FlexLayoutModule,
+	HttpModule,
+	FormsModule
+	
+	
   ],
-  providers: [],
+  providers: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
